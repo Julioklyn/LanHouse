e53abc9aa4d477f6fa890a4c3810e557db8f409f
Julioklyn <julio.carlos@aluno.ifsp.edu.br>
2025-04-29 17:44:28 -0300

    Barra de Busca 🔎

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,39 @@ document.addEventListener("DOMContentLoaded", function () {
             parent.classList.toggle('active');
         });
     });
+
+    //FECHAR O DROPDPWN AO CLICAR FORA
+    document.addEventListener('click', function (e) {
+        dropdownToggles.forEach((toggle) => {
+            const parent = toggle.closest('.dropdown-item');
+            if (!parent.contains(e.target)) {
+                parent.classList.remove('active');
+            }
+        })
+    })
+
+});
+
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const searchClose = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click',  () =>{
+    searchOverlay.style.display = 'flex'; 
+    setTimeout(() => {
+        searchOverlay.querySelector('.search-input').focus();
+    }, 100);
+});
+
+//fechar o botao
+
+closeSearch.addEventListener('click', () => {
+    searchOverlay.style.display = 'none';
+})
+
+//fechar cpm o ESC
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        searchOverlay.style.display = 'none';
+    }
 });
